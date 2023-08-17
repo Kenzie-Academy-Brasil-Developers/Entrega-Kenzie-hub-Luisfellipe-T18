82c4b4d1462873e33258fe7984b8d4bf523893e1
LuisFellipeT18 <luisfellipe.silva@outlook.com>
2023-08-17 10:34:47 -0500

    Entrega Sprint 5

--- src/providers/UserContext.jsx
+++ src/providers/UserContext.jsx
@@ -1,100 +1,99 @@
-import { createContext, useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { api } from "../services/api"
-import { toast } from "react-toastify"
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../services/api";
+import { toast } from "react-toastify";
 
-export const UserContext = createContext({})
+export const UserContext = createContext({});
 
-export const UserProvider = ({children}) => {
-    const [user, setUser] = useState(null)
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-    const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    useEffect(() => {
+  const pathname = window.location.pathname;
+  
 
-        const userID = localStorage.getItem("@USERID")
-        const token = localStorage.getItem("@TOKEN")
+  useEffect(() => {
+    const userID = localStorage.getItem("@USERID");
+    const token = localStorage.getItem("@TOKEN");
 
-        const getUser = async () => {
-            try {
-                setLoading(true)
-                const { data } = await api.get(`users/${userID}`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
+    const getUser = async () => {
+      try {
+        setLoading(true);
+        const { data } = await api.get(`users/${userID}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUser(data);
+        navigate(pathname);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (userID && token) {
+      getUser();
+    }
+  }, []);
 
-                    }
-                })
-                setUser(data)
-                navigate("/dashboard")
-               
-            } catch (error) {
-                console.log(error);
-            }finally {
-                setLoading(false)
-            }
-        }
-        if(userID && token){
-
-            getUser();
-        }
-    }, [])
-
-    const userLogout = () => {
-        setUser(null)
-        navigate("/")
-        localStorage.removeItem("@USERID")
-        localStorage.removeItem("@TOKEN")
-        toast.warn("Deslogando"), {
+  const userLogout = () => {
+    setUser(null);
+    navigate("/");
+    localStorage.removeItem("@USERID");
+    localStorage.removeItem("@TOKEN");
+    toast.warn("Deslogando"),
+      {
         autoClose: 1000,
-        }
-      }
-
-      const userLogin = async(formData, setLoading, reset) => {
-        try {
-          setLoading(true)
-          const { data } = await api.post("/sessions", formData)
-          setUser(data.user)
-          localStorage.setItem("@USERID", data.user.id)
-          localStorage.setItem("@TOKEN", data.token)
-          reset();
-          navigate("/dashboard")
-          toast.success("Login realizado com sucesso", {
-            autoClose: 2000,
-          })
-          
-        } catch (error) {
-            toast.error("Não foi possível efectuar o login", {
-             autoClose: 2000,
-            })
-          
-        } finally {
-          setLoading(false)
-        }
-      }
-
-      const userRegister = async (formData, setLoading) => {
-        try {
-          setLoading(true);
-          await api.post("/users", formData);
-          toast.success("Cadastro criado com sucesso", {
-            autoClose: 2000,
-            
-          })
-          navigate("/");
-        } catch  {
-         toast.error("Não foi possível efectuar o cadastro", {
-            autoClose: 2000,
-           })
-        } finally {
-          setLoading(false);
-        }
       };
+  };
 
-    return (
-        <UserContext.Provider value={{ loading, user,  userLogin, userRegister, userLogout }}>
-            {children}
-        </UserContext.Provider>
-    )
-}
+  const userLogin = async (formData, setLoading, reset) => {
+    try {
+      setLoading(true);
+      const { data } = await api.post("/sessions", formData);
+      setUser(data.user);
+      localStorage.setItem("@USERID", data.user.id);
+      localStorage.setItem("@TOKEN", data.token);
+      reset();
+      navigate("/dashboard");
+      toast.success("Login realizado com sucesso", {
+        autoClose: 2000,
+      });
+    } catch (error) {
+      toast.error("Não foi possível efectuar o login", {
+        autoClose: 2000,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const userRegister = async (formData, setLoading) => {
+    try {
+      setLoading(true);
+      await api.post("/users", formData);
+      toast.success("Cadastro criado com sucesso", {
+        autoClose: 2000,
+      });
+      navigate("/");
+    } catch {
+      toast.error("Não foi possível efectuar o cadastro", {
+        autoClose: 2000,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <UserContext.Provider
+      value={{ loading, user, userLogin, userRegister, userLogout }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,15 +3,22 @@ import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Dashboard } from "../pages/Dashboard";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { PublicRoutes } from "./PublicRoutes";
 
 export const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route element={<PrivateRoutes/>}>
+
+      <Route element={<PublicRoutes />}>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+
+      <Route element={<PrivateRoutes />}>
         <Route path="/dashboard" element={<Dashboard />} />
       </Route>
+
+
     </Routes>
   );
 };
